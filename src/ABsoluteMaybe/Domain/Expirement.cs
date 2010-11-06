c13081d79ff835178170e008d1ded142f04cf21e
Troy Goode <troygoode@gmail.com>
2010-11-06 13:47:31 -0400

    Expirement now able to calculate some basic aggregations on the fly.

--- src/ABsoluteMaybe/Domain/Expirement.cs
+++ src/ABsoluteMaybe/Domain/Expirement.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ABsoluteMaybe.Domain
 {
 	public class Expirement
 	{
-		public Expirement(string name, string conversionKeyword, DateTime dateCreated, DateTime? dateEnded, IEnumerable<ParticipationRecord> participants)
+		public Expirement(string name, string conversionKeyword, DateTime dateCreated, DateTime? dateEnded,
+		                  IEnumerable<ParticipationRecord> participants)
 		{
 			Name = name;
 			ConversionKeyword = conversionKeyword;
@@ -19,5 +21,41 @@ namespace ABsoluteMaybe.Domain
 		public DateTime DateCreated { get; private set; }
 		public DateTime? DateEnded { get; private set; }
 		public IEnumerable<ParticipationRecord> Participants { get; private set; }
+
+		private IEnumerable<Option> _options;
+		public IEnumerable<Option> Options
+		{
+			get
+			{
+				return _options ?? (_options = Participants
+				                               	.GroupBy(p => p.AssignedOption)
+				                               	.Select(g => new Option(g.Key,
+				                               	                        g.Count(),
+				                               	                        g.Count(p => p.HasConverted))));
+			}
+		}
+
+		#region Nested type: Option
+
+		public class Option
+		{
+			internal Option(string name, int participants, int conversions)
+			{
+				Name = name;
+				Participants = participants;
+				Conversions = conversions;
+			}
+
+			public string Name { get; private set; }
+			public int Participants { get; private set; }
+			public int Conversions { get; private set; }
+
+			public double ConversionRate
+			{
+				get { return (double) Conversions/Participants; }
+			}
+		}
+
+		#endregion
 	}
 }
---
+++ src/AbsoluteMaybe.Tests/Domain/ExpirementTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using ABsoluteMaybe.Domain;
+using NUnit.Framework;
+using Should;
+
+namespace AbsoluteMaybe.Tests.Domain
+{
+	[TestFixture]
+	public class ExpirementTests
+	{
+		[Test]
+		public void OptionsAreGroupedProperly()
+		{
+			//arrange
+			var participants = new[]
+			                   	{
+			                   		new ParticipationRecord("user1", "OPTION_1", false, null),
+									new ParticipationRecord("user2", "OPTION_1", false, null),
+									new ParticipationRecord("user1", "OPTION_2", false, null),
+									new ParticipationRecord("user2", "OPTION_2", false, null),
+			                   	};
+			var exp = new Expirement(null, null, DateTime.Now, null, participants);
+
+			//act
+			var options = exp.Options;
+
+			//assert
+			options.Count().ShouldEqual(2);
+		}
+
+		[Test]
+		public void OptionParticipantsAreCountedProperly()
+		{
+			//arrange
+			var participants = new[]
+			                   	{
+			                   		new ParticipationRecord("user1", "OPTION_1", false, null),
+									new ParticipationRecord("user2", "OPTION_1", false, null),
+									new ParticipationRecord("user1", "OPTION_2", false, null),
+									new ParticipationRecord("user2", "OPTION_2", false, null),
+									new ParticipationRecord("user3", "OPTION_2", false, null),
+			                   	};
+			var exp = new Expirement(null, null, DateTime.Now, null, participants);
+
+			//act
+			var options = exp.Options;
+
+			//assert
+			options.ElementAt(0).Participants.ShouldEqual(2);
+			options.ElementAt(1).Participants.ShouldEqual(3);
+		}
+
+		[Test]
+		public void OptionConversionsAreCountedProperly()
+		{
+			//arrange
+			var participants = new[]
+			                   	{
+			                   		new ParticipationRecord("user1", "OPTION_1", false, null),
+									new ParticipationRecord("user2", "OPTION_1", false, null),
+									new ParticipationRecord("user1", "OPTION_2", false, null),
+									new ParticipationRecord("user2", "OPTION_2", true, DateTime.Now),
+									new ParticipationRecord("user3", "OPTION_2", false, null),
+			                   	};
+			var exp = new Expirement(null, null, DateTime.Now, null, participants);
+
+			//act
+			var options = exp.Options;
+
+			//assert
+			options.ElementAt(0).Conversions.ShouldEqual(0);
+			options.ElementAt(1).Conversions.ShouldEqual(1);
+		}
+
+		[Test]
+		public void OptionConversionRateIsCountedProperly()
+		{
+			//arrange
+			var participants = new[]
+			                   	{
+			                   		new ParticipationRecord("user1", "OPTION_1", false, null),
+									new ParticipationRecord("user2", "OPTION_1", true, DateTime.Now),
+									new ParticipationRecord("user3", "OPTION_1", false, null),
+									new ParticipationRecord("user4", "OPTION_1", true, DateTime.Now)
+			                   	};
+			var exp = new Expirement(null, null, DateTime.Now, null, participants);
+
+			//act
+			var options = exp.Options;
+
+			//assert
+			options.ElementAt(0).ConversionRate.ShouldEqual(.5);
+		}
+	}
+}
